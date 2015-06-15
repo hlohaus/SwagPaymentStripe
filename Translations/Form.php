ad564417fe5dea942a6d96064ef72df0739e13cf
Heiner Lohaus <hl@shopware.com>
2015-06-15 12:33:52 +0200

    PT-3102 Code review, add form/payment translations

---
+++ Translations/Form.php
@@ -0,0 +1,11 @@
+<?php
+return array('en_GB' => array(
+    'stripeSecretKey' => array(
+        'label' => 'Stripe Secret Key',
+        'description' => 'Enter here your secret key.',
+    ),
+    'stripePublishableKey' => array(
+        'label' => 'Stripe Publishable Key',
+        'description' => 'Enter here your publishable key.',
+    )
+));
---
+++ Translations/Payment.php
@@ -0,0 +1,5 @@
+<?php
+return array('en_GB' => array(
+    'description' => 'Credit card by Stripe',
+    'additionalDescription' => ''
+));
